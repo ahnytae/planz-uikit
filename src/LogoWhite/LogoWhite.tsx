a29929596c79fbb1c17aa8efc144f19e99bfe38f
MJ Kim <58619071+howdy-mj@users.noreply.github.com>
2020-06-08 13:40:26 +0900

    [DS-4] 화이트 로고 추가
    what?
    화이트 로고 컴포넌트 추가
    
    how?
    planz-logo white 이미지 파일을 스토리북에 올림
    
    why?
    디자인 시스템 구축

---
+++ src/LogoWhite/LogoWhite.tsx
@@ -0,0 +1,23 @@
+/** @jsx jsx */
+import { jsx, css } from '@emotion/core';
+import logoImg from './image/logo_white.png';
+
+export type LogoWhiteProps = {
+  name: string;
+};
+
+/** LogoWhite 컴포넌트 */
+const LogoWhite = ({ name }: LogoWhiteProps): unknown => {
+  return (
+    <div>
+      <h1>{name}</h1>
+      <img src={logoImg} css={logoSize} />
+    </div>
+  );
+};
+
+const logoSize = css`
+  width: 500px;
+`;
+
+export default LogoWhite;
